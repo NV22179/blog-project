72d89392df43336fc8b2d159b7226b71866fbf74
NV22179 <147533054+NV22179@users.noreply.github.com>
2024-03-08 12:35:25 +0000

    Updates to - 11

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainLayout from '../../components/MainLayout'
+import Articles from './container/Articles'
+import Hero from './container/Hero'
+import CTA from './container/CTA'
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+      <Hero />
+      <Articles />
+      <CTA />
+    </MainLayout>
+  );
+};
+
+export default HomePage;
